1fbd47d1bb0c866f5ba0e04c21eab8654cda9aaa
chenhongjin <stellaginc@163.com>
2014-11-17 23:13:37 +0800

    refresh

---
+++ AddTopicActivity.java
@@ -0,0 +1,175 @@
+package com.example.note;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.note.domain.Topic;
+import com.note.service.remoteURL;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class AddTopicActivity extends Activity{
+	static remoteURL remote = new remoteURL();
+    private static  String processURL=remote.remoteURL+"addTopic.action?";
+	private final String processURL_constant  = remote.remoteURL+"addTopic.action?";
+	private final String processURL_findstring =remote.remoteURL+"jfindString.action?";
+	String result=null;
+	Button button;
+	EditText title;
+	EditText note;
+	EditText conclusion;
+	EditText site;
+	EditText member;
+	String res[]=null;
+	ArrayAdapter<String> adapter;
+	MultiAutoCompleteTextView multiautoCompleteTextView;
+    public void onCreate(Bundle savedInstanceState){
+  	  super.onCreate(savedInstanceState);
+		  setContentView(R.layout.addtopic);
+		  multiautoCompleteTextView = (MultiAutoCompleteTextView) findViewById(R.id.member);
+		  multiautoCompleteTextView.setThreshold(1);
+		  multiautoCompleteTextView.addTextChangedListener(new TextWatcher(){
+			  public void onTextChanged(CharSequence s, int start, int before, int count){
+	              String str = s.toString();    
+	              System.out.println(str);
+	              int len = s.length();
+	              if(len!=0){
+	            	if(str.charAt(len-1)!=' '){
+	            		String [] tmp=str.split(", ");
+	                    String [] temp=remote.getRemoteString(processURL_findstring,tmp[tmp.length-1]);
+	                    if(temp!=null){
+	                    adapter = new ArrayAdapter<String>(AddTopicActivity.this,  
+	  	                android.R.layout.simple_dropdown_item_1line, temp); 
+	                    multiautoCompleteTextView.setAdapter(adapter);
+	                    Log.d("mylog","multiauto");
+	                    Log.d("mylog",temp[0]);
+	                    multiautoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+	            	}
+	              }
+                  }
+			  }
+		      public void beforeTextChanged(CharSequence s, int start, int count,  
+		                int after) {  
+		      }  
+		      public void afterTextChanged(Editable s) {  
+		      }  
+		  });
+		  findViews();  
+    }
+    private void findViews(){
+   	   Intent intent = this.getIntent();
+   	   Bundle bundle = intent.getExtras();
+   	   final String idstr = bundle.getString("id");
+   	   Log.d("bundletest",idstr);
+       button = (Button)findViewById(R.id.publish);
+  	   title  = (EditText)findViewById(R.id.title);
+  	   note = (EditText)findViewById(R.id.note);
+  	   conclusion= (EditText)findViewById(R.id.conclusion);
+  	   site = (EditText)findViewById(R.id.site);
+  	   member  = (EditText)findViewById(R.id.member);
+  	   button.setOnClickListener(new OnClickListener(){
+  		   public void onClick(View v){
+  			   String titlestr=title.getText().toString().trim();
+  			   String notestr=note.getText().toString();
+  			   String  conclusionstr=conclusion.getText().toString();
+  			   Log.d("mylog",conclusionstr);
+  			   String sitestr=site.getText().toString().trim();
+  			 String memberstr=member.getText().toString().trim();
+  			 addTopicRemoteService(titlestr,notestr,conclusionstr,idstr,sitestr,memberstr);
+               /*TopicService topicservice = new TopicService(AddTopicActivity.this);
+               Topic topic = new Topic(userid,titlestr,notestr,conclusionstr);
+               boolean f = topicservice.insert(topic);
+               if(f){
+            	   Log.i("TAG","发布成功!");  
+            	   Toast.makeText(AddTopicActivity.this, "发布成功！", Toast.LENGTH_LONG).show();
+               }*/
+  			Intent intent1=new Intent(AddTopicActivity.this,ClassNoteActivity.class);//启动register活动
+			Bundle bundle1= new Bundle();
+			bundle1.putString("id", idstr);
+			intent1.putExtras(bundle1);
+			startActivity(intent1);    
+  		   }
+  	   });
+    }
+	private void addTopicRemoteService(String title,String note,
+			 String conclusion,String userid,String site,String member){
+    	try {
+   		    String date=getDate();
+   		    date=java.net.URLEncoder.encode(date,"utf-8");
+   		    title=java.net.URLEncoder.encode(title,"utf-8");
+   		    note=java.net.URLEncoder.encode(note,"utf-8");
+   		    conclusion=java.net.URLEncoder.encode(conclusion,"utf-8");
+   		    site=java.net.URLEncoder.encode(site,"utf-8");
+	    	HttpClient httpclient = new DefaultHttpClient();
+	    	processURL= processURL_constant+"title="+title+"&note="+note+
+	    			"&conclusion="+conclusion+"&date="+date+"&userid="+userid
+	    			+"&site="+site+"&member="+member;
+	    	HttpPost request=new HttpPost(processURL);
+	    	request.addHeader("Accept","text/json");
+			HttpResponse response =httpclient.execute(request);
+			HttpEntity entity=response.getEntity();
+			String json =EntityUtils.toString(entity,"UTF-8");
+			if(json!=null){
+				JSONObject jsonObject=new JSONObject(json);
+				result=jsonObject.get("message").toString().trim();
+			}
+		   if(result==null){ 
+			   json="通信出错！";
+		   }
+			//创建提示框提醒是否登录成功
+		   Log.d("mylog","AlertDialog");
+			 //AlertDialog.Builder builder=new Builder(AddTopicActivity.this);
+			 //builder.setTitle("提示")
+			 //.setMessage(result)
+			 //.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+				
+				//public void onClick(DialogInterface dialog, int which) {
+				//	dialog.dismiss();
+				//}
+			//}).create().show();
+		 
+    	 } catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private String getDate(){
+		java.util.Date date = new java.util.Date();
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String res = df.format(date);
+		return res;
+	}
+}
---
+++ ClassNoteActivity.java
@@ -0,0 +1,199 @@
+package com.example.note;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+//import net.sf.json.JSONArray;
+
+
+
+
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.example.note.R;
+import com.note.domain.Topic;
+import com.note.service.TopicDatabaseHelper;
+import com.note.service.UserService;
+import com.note.service.remoteURL;
+import com.example.note.R;
+import com.example.note.AddTopicActivity;
+import com.example.note.UserInfoActivity;
+public class ClassNoteActivity extends ListActivity{
+	  static remoteURL remote = new remoteURL();
+	  private   String processURL=remote.remoteURL+"getTopicList.action?";
+	  private final String processURL_findTopicList=remote.remoteURL+"getTopicList.action?";
+	  private final String processURL_myTopicList=remote.remoteURL+"myTopicList.action?";
+	  EditText searchtext;
+	  Button   addtopic;
+	  Button   userinfo;
+	  Button search;
+	  String userid;
+	  String result=null;
+      public void onCreate(Bundle savedInstanceState){
+    	  super.onCreate(savedInstanceState);
+    	  this.setTitle("课堂讨论笔记");
+  		  setContentView(R.layout.note);
+    	  Intent intent0 = this.getIntent();
+    	  final Bundle bundle=intent0.getExtras();
+    	  userid = bundle.getString("id");
+    	  final String URL= processURL_findTopicList+"userid="+userid;
+  		  findViews(bundle); 
+  		  remote(URL);
+      }
+      public void onRestart(Bundle savedInstanceState){
+    	  setContentView(R.layout.note);
+    	  super.onRestart();
+    	  Bundle bundle1 = getIntent().getExtras();
+    	  String data = bundle1.getString("Data");
+    	  Log.d("mylog","onRestart");
+    	  Log.d("bundle",data);
+    	  final String URL= processURL_findTopicList+"userid="+data;
+  		  remote(URL);
+      }
+      public void OnActivityResult(int requestCode, int resultCode, Intent data){
+    	  setContentView(R.layout.note);
+    	  Log.d("mylog","onActivityResult");
+    	  super.onActivityResult(requestCode, resultCode, data);
+    	  final String URL= processURL_findTopicList+"userid="+userid;
+  		  remote(URL);  
+      }
+      private void findViews(final Bundle bundle){
+    	  addtopic = (Button) findViewById(R.id.addtopic);
+    	  userinfo = (Button) findViewById(R.id.userinfo);
+    	  searchtext = (EditText) findViewById(R.id.searchtext);
+    	  search = (Button) findViewById(R.id.search);
+  		  addtopic.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent=new Intent(ClassNoteActivity.this,AddTopicActivity.class);//启动register活动
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+		});
+  		  userinfo.setOnClickListener(new OnClickListener(){
+  			  public void onClick(View v){
+  				  Intent intent1=new Intent(ClassNoteActivity.this,UserInfoActivity.class);
+  				  intent1.putExtras(bundle);
+  				  startActivity(intent1);
+  			  }
+  		  });
+  		 search.setOnClickListener(new OnClickListener(){
+  			  public void onClick(View v){
+  				  String searchstr = searchtext.getText().toString().trim();
+  				  String URL_myTopic=processURL_myTopicList+"searchstr="+searchstr;
+  				  remote(URL_myTopic);
+  			  }
+  		  });
+      }
+      private void showlist(ArrayList<Topic> list){//显示笔记列表
+       	  String[] from = {"title","note","date"};
+       	  int[] to = {R.id.title,R.id.note,R.id.date};
+       	  SimpleAdapter adapter = new SimpleAdapter(this,
+       			  getData(list),R.layout.topiclist,from,to);
+       	  
+       	  ListView listview = getListView();
+       	  listview.setAdapter(adapter);//添加适配器
+       	  listview.setOnItemClickListener(new OnItemClickListener(){
+       		  public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+       			    //Log.i("mylog", "item class : "+ ((ListView) parent).
+       			     //getItemAtPosition(position).getClass().getSimpleName());
+       		          HashMap<String, String>  map = (HashMap<String, String>) ((ListView) parent).
+       				 getItemAtPosition(position);  
+       		         String  topic_id=map.get("id");
+       		         Intent notelist = new Intent(ClassNoteActivity.this,NoteListActivity.class);
+       		         Bundle notebundle = new Bundle();
+       		         notebundle.putString("topic_id", topic_id);
+       		         notebundle.putString("userid", userid);
+       		         notelist.putExtras(notebundle);
+       		         startActivity(notelist);
+       		  }
+       	  });
+         }
+      private void remote(String url){
+      	try{
+    	    HttpClient httpclient = new DefaultHttpClient();
+            HttpPost request=new HttpPost(url);
+    	    request.addHeader("Accept","text/json");
+		    HttpResponse response =httpclient.execute(request);
+		    HttpEntity entity=response.getEntity();
+		    String json =EntityUtils.toString(entity,"UTF-8");
+		    if(json!=null){
+				JSONObject jsonObject=new JSONObject(json);
+				result=jsonObject.get("TopicList").toString().trim();
+		    	JSONArray jsonarray = new JSONArray(result);
+		    	int len = jsonarray.length();
+		    	ArrayList<Topic> list = new ArrayList<Topic>();
+		    	for(int i=0;i<len;++i){
+		            JSONObject obj = jsonarray.getJSONObject(i);
+		            String title = obj.getString("title");
+		            String member= obj.getString("member");
+		            String site  = obj.getString("site");
+		            String date = obj.getString("date");
+		            String note = obj.getString("note");
+		            String conclusion = obj.getString("conclusion");
+		            String userid = obj.getString("userid");
+		            String id= obj.getString("id");
+		            Topic topic = new Topic(userid,title,note,conclusion,date,site
+		            		,member,id);
+		            list.add(topic);
+		    	}
+		    	showlist(list);
+		    }else{
+		    	Log.d("mylog","json=null");
+		    }
+   	    } catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch(JSONException e){
+			e.printStackTrace();
+		}
+      }
+  	  private List<Map<String, Object>> getData(ArrayList<Topic> list1) {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        int len = list1.size();
+        
+        for(int i=0;i<len;++i){
+        	Map<String, Object> map = new HashMap<String, Object>();
+        	map.put("title", list1.get(i).getTitle());        	
+        	String note = list1.get(i).getNote();
+        	if(note.length()>100) note=note.substring(0, 99)+"……";
+        	map.put("note", note);
+        	map.put("date", list1.get(i).getDate());
+        	map.put("id", list1.get(i).getId());
+        	list.add(map);
+        }
+		return list;
+	}
+}
